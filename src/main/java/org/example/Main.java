33d3c7984de8e570f6715a9e734fb48e78276414
Dex Joseph <dex@mac-mini.mynetworksettings.com>
2023-03-03 14:52:16 -0500

    lots of stuff, will explain in comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,53 +2,87 @@ package org.example;
 
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 
-import java.awt.image.AreaAveragingScaleFilter;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
-        String endpoint = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:aspirin&limit=10";
+    public static void main(String[] args) throws Exception {
 
-        HttpGet request = new HttpGet(endpoint);
+        String apiUrl = "https://api.fda.gov/drug/label.json?&limit=50";
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpResponse response = httpClient.execute(request);
 
-        if(response.getCode() != 200){
-            throw new RuntimeException("Failed to get the shit " + response.getCode());
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            JsonObject jsonObject = new Gson().fromJson(response.toString(), JsonObject.class);
+            JsonArray results = jsonObject.getAsJsonArray("results");
+
+            Connection jDbCconnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dex");
+            PreparedStatement statement = jDbCconnection.prepareStatement("INSERT INTO medication_test (brand_name) VALUES (?)");
+
+
+
+                List<String> brandNameList = new ArrayList<>();
+                List<Medication> medications = new ArrayList<>();
+                for (JsonElement resultElement : results) {
+                    JsonObject result = resultElement.getAsJsonObject();
+                    JsonArray brandNames = result.getAsJsonObject("openfda").getAsJsonArray("brand_name");
+                    if (brandNames == null) {
+                        continue;
+                    }
+                    for (JsonElement brandNameElement : brandNames) {
+                        String brandName = brandNameElement.getAsString();
+
+                        Medication medication = new Medication();
+                        medication.setBrand_name(brandName);
+                        medications.add(medication);
+
+                        statement.setString(1, brandName);
+                        statement.execute();
+                    }
+                }
+                for (String brand : brandNameList) {
+                    System.out.println(brand);
+                }
+
+            statement.close();
+            jDbCconnection.close();
+        } else {
+            System.out.println("Error retrieving medication data. Response code: " + responseCode);
         }
-        String jsonResponse = EntityUtils.toString(((ClassicHttpResponse) response).getEntity());
-        JsonObject jsonObject = new Gson().fromJson(jsonResponse, JsonObject.class);
 
-        //Create list of Medication objects
-        List<Medication> medications = new ArrayList<>();
-
-        for(int i = 0; i < jsonObject.getAsJsonArray("results").size(); i++){
-            String brandName = jsonObject.getAsJsonArray("results").get(i)
-                    .getAsJsonObject().getAsJsonObject("patient")
-                    .getAsJsonArray("drug").get(0).getAsJsonObject().getAsJsonArray("openfda")
-                    .get(0).getAsString();
-
-            Medication medication = new Medication();
-            medication.setBrand_name(brandName);
-            medications.add(medication);
-        }
-
-        //printing them out
-        for(Medication medication : medications){
-            System.out.println(medication.getBrand_name());
-        }
     }
 }
--- src/main/java/org/example/Medication.java
+++ src/main/java/org/example/Medication.java
@@ -2,12 +2,22 @@ package org.example;
 
 public class Medication {
   private String brand_name;
+
+  private String purpose;
     public String getBrand_name() {
         return brand_name;
     }
 
     public void setBrand_name(String brand_name) {
         this.brand_name = brand_name;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
     }
 
 
