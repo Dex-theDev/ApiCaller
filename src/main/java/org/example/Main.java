62ff64cf3cd8fa25d33d74f3ef7ffa6ae950af03
Dex Joseph <dex@dexs-mac-mini.mynetworksettings.com>
2023-03-15 12:52:28 -0400

    halfway working, need to handle possible NPE's in results

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,10 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+
 
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -30,7 +21,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        String apiUrl = "https://api.fda.gov/drug/label.json?&limit=50";
+        String apiUrl = "https://api.fda.gov/drug/drugsfda.json?&limit=500";
 
 
         URL url = new URL(apiUrl);
@@ -50,8 +41,10 @@ public class Main {
             JsonObject jsonObject = new Gson().fromJson(response.toString(), JsonObject.class);
             JsonArray results = jsonObject.getAsJsonArray("results");
 
+            Class.forName("org.postgresql.Driver");
+
             Connection jDbCconnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dex");
-            PreparedStatement statement = jDbCconnection.prepareStatement("INSERT INTO medication_test (brand_name) VALUES (?)");
+            PreparedStatement statement = jDbCconnection.prepareStatement("INSERT INTO medication_test2 (brand_name, strength, dosage_form) VALUES (?,?,?)");
 
 
 
@@ -59,26 +52,44 @@ public class Main {
                 List<Medication> medications = new ArrayList<>();
                 for (JsonElement resultElement : results) {
                     JsonObject result = resultElement.getAsJsonObject();
-                    JsonArray brandNames = result.getAsJsonObject("openfda").getAsJsonArray("brand_name");
-                    if (brandNames == null) {
+                    JsonArray products = result.getAsJsonArray("products");
+                    String brandName = products.getAsJsonArray().get(0).getAsJsonObject().getAsJsonPrimitive("brand_name").getAsString();
+                    String strength = products.getAsJsonArray().get(0).getAsJsonObject().getAsJsonArray("active_ingredients").getAsJsonArray().get(0).getAsJsonObject().getAsJsonPrimitive("strength").getAsString();
+                    String dosage_form = products.getAsJsonArray().get(0).getAsJsonObject().getAsJsonPrimitive("dosage_form").getAsString();
+                    if (brandName == null) {
                         continue;
                     }
-                    for (JsonElement brandNameElement : brandNames) {
-                        String brandName = brandNameElement.getAsString();
+                    brandNameList.add(brandName);
 
-                        Medication medication = new Medication();
-                        medication.setBrand_name(brandName);
-                        medications.add(medication);
+                    Medication medication = new Medication();
+                    medication.setBrand_name(brandName);
+                    medication.setStrength(strength);
+                    medication.setDosage_form(dosage_form);
+                    medications.add(medication);
 
-                        statement.setString(1, brandName);
-                        statement.execute();
-                    }
+                    statement.setString(1, brandName);
+                    statement.setString(2, strength);
+                    statement.setString(3, dosage_form);
+                    statement.execute();
+//                    for (JsonElement brandNameElement : brandName) {
+//                       // String brandName = brandNameElement.getAsString();
+//
+//                        Medication medication = new Medication();
+//                      //  medication.setBrand_name(brandName);
+//                        medications.add(medication);
+//
+//                       // statement.setString(1, brandName);
+//                       // statement.execute();
+//                    }
                 }
                 for (String brand : brandNameList) {
                     System.out.println(brand);
                 }
+                for(Medication medication : medications){
+                    System.out.println(medication.toString());
+                }
 
-            statement.close();
+           statement.close();
             jDbCconnection.close();
         } else {
             System.out.println("Error retrieving medication data. Response code: " + responseCode);
